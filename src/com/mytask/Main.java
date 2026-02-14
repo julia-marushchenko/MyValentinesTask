83029fc95dd92ada531a5082ebaa0d33f9e3f337
Julia Marushchenko <marushchenko.yulia@gmail.com>
2026-02-14 18:03:43 +0100

    Add files via upload
    Committing Main class.

---
+++ src/com/mytask/Main.java
@@ -0,0 +1,36 @@
+/**
+ * Java program to demonstrate System.out.
+ */
+
+package com.mytask;
+
+/**
+ *  Main class.
+ */
+public class Main {
+
+    // Main method to drive java program.
+    public static void main(String[] args) {
+
+        // Calling print() method of System class.
+        System.out.println("          #####                  #####" );
+        System.out.println("       ###########             ###########" );
+        System.out.println("    ####         ####       ####         ####");
+        System.out.println("   ###              ##    ##                ###");
+        System.out.println("  ###      #         ##  ##                  ###");
+        System.out.println(" ###      #            ##                    ###");
+        System.out.println(" ###     ##                                 ####");
+        System.out.println(" ####     #                                #####");
+        System.out.println("  ####     #                              ####");
+        System.out.println("    ####                                 ####");
+        System.out.println("     ####                              ####");
+        System.out.println("       ####                          ####");
+        System.out.println("         ####                     ####");
+        System.out.println("            ###                 ###");
+        System.out.println("               ###           ###");
+        System.out.println("                  ###     ###");
+        System.out.println("                      ###");
+        System.out.println("                       #");
+
+    }
+}
